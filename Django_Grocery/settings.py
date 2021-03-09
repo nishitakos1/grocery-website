f29bdf3def98a4c6c51fd1aeb6297b754f285041
nishitakos1 <54737258+nishitakos1@users.noreply.github.com>
2021-03-09 08:35:48 +0530

    Update settings.py

--- Django_Grocery/settings.py
+++ Django_Grocery/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v=@!_%0^7x($@%n=w0o*gs#p58r#*djnqwiem#*28vl*r9e=^!'
+SECRET_KEY = '****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
